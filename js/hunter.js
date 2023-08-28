e7f1b2d4720436b038e3ce4356cc611171284d91
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-28 15:47:01 +0600

    modal and single phone data showing done

--- js/hunter.js
+++ js/hunter.js
@@ -39,8 +39,8 @@ const displayPhones = (phones, isShowAll) =>{
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
 
@@ -70,6 +70,36 @@ const toggleLoadingSpinner = (isLoading) =>{
   else{
     loadingSpinner.classList.add('hidden')
   }
+}
+
+// handle show details 
+const handleShowDetail = async (id) =>{
+   const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+   const data = await res.json();
+   const phone = data.data;
+   showPhoneDetails(phone);
+}
+
+// show phone details 
+const showPhoneDetails = (phone) =>{
+  console.log(phone)
+
+  const modalContainer = document.getElementById('modal-container');
+  modalContainer.innerHTML = `
+  <img class="w-[100px] mx-auto mb-8 bg-gray-200" src="${phone.image}" alt =""/>
+  <h3><span class="text-lg font-semibold">Brand: </span>${phone.brand}</h3>
+  <h3><span class="text-lg font-semibold">Name: </span>${phone.name}</h3>
+  <h3><span class="text-lg font-semibold">Chipset: </span>${phone.mainFeatures.chipSet  }</h3>
+  <h3><span class="text-lg font-semibold">Memory: </span>${phone.mainFeatures.memory  }</h3>
+  <h3><span class="text-lg font-semibold">Sensor: </span>${phone.mainFeatures.sensors.map(sensor => sensor) }</h3>
+  <h3><span class="text-lg font-semibold">GPS: </span>${phone.others.GPS }</h3>
+  <h3><span class="text-lg font-semibold">NFC: </span>${phone.others.NFC }</h3>
+  <h3><span class="text-lg font-semibold">USB: </span>${phone.others.USB }</h3>
+  <h3><span class="text-lg font-semibold">Radio: </span>${phone.others.Radio }</h3>
+  `
+
+// show modal 
+show_details_modal.showModal()
 }
 
 // handle show all 
